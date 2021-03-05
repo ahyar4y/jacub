35de248e07e63cdcb9f510af361f7ca40057dc8c
ahyar4y <ahyar4y08@gmail.com>
2021-03-05 21:46:24 +0700

    update command ifs into dynamic command execution

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const { prefix, token } = require("./config.json");
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
-client.once("ready", () => {
-    console.log("ready");
-    client.user.setPresence({ activity: { name: ".whoru", type: "LISTENING" } });
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('ready');
+    client.user.setPresence({ activity: { name: '.cmds', type: 'LISTENING' } });
 });
 
-client.on("message", async (message) => {
+client.on('message', async (message) => {
     if (!message.content.startsWith(prefix)) return;
 
     const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-    switch (command) {
-        case 'whoru':
-            message.channel.send(`Just Another Casual Utilty Bot, ${message.author}`);
-            break;
-        case 'whoami':        
-            message.channel.send(`${message.author.username}\n${message.author.id}\n${message.author.createdAt}\n${message.author.displayAvatarURL({ format: 'png', dynamic: true })}`);
-            break;
-        case 'whois':
-            const taggedUser = message.mentions.users.first();
-            if (!taggedUser) return message.channel.send('who?');  
-            message.channel.send(`${taggedUser.username}\n${taggedUser.id}\n${taggedUser.createdAt}\n${taggedUser.displayAvatarURL({ format: 'png', dynamic: true })}`);
-            break;
-        case 'join':
-            if (message.member.voice.channel) await message.member.voice.channel.join();
-            break;
-        case 'leave':
-            if (message.guild.voice.channel) message.member.voice.channel.leave();
+    const cmdName = args.shift().toLowerCase();
+
+    if (!client.commands.has(cmdName)) return;
+
+    const command = client.commands.get(cmdName);
+    
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error executing that command');
     }
 });
 
