79b5a9ba37ea5a49ee245af6b6315b2bec3f6599
ahyar4y <ahyar4y08@gmail.com>
2021-03-07 13:13:40 +0700

    update join code

--- commands/join.js
+++ commands/join.js
@@ -2,6 +2,13 @@ module.exports = {
     name: 'join',
     description: 'join',
     async execute(message, args) {
-        if (message.member.voice.channel) await message.member.voice.channel.join();
+        const voiceChannel = message.member.voice.channel;
+        if (voiceChannel) {
+            if (message.client.voice.connections.some(conn => conn.channel.id === voiceChannel.id)) {
+                return message.channel.send('Already in');
+            }
+            await message.member.voice.channel.join();
+        }
     }
+    
 }
