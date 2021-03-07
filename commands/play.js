d2bc9a179959c10caf5b1da4546e3544abde4e83
ahyar4y <ahyar4y08@gmail.com>
2021-03-07 13:14:36 +0700

    update .play code

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,41 @@
+const { TeamMember } = require('discord.js');
+
 module.exports = {
     name: 'play',
     description: 'play',
-    execute(message, args) {
+    async execute(message, args) { // initial code, still takes long to play.
+        const {google} = require('googleapis');
+        const {youtubeAPI} = require('../config.json');
+        const youtube = google.youtube({
+            version: 'v3',
+            auth: youtubeAPI
+        });
+
+        const query = args.join(' ');
         
+        message.channel.send(`Searching for \`${query}\``);
+
+        const res = await youtube.search.list({
+            part: 'id, snippet',
+            q: query,
+            type: 'video'
+        });
+        
+        const vid = res.data.items[0];
+
+        const connection = await message.member.voice.channel.join(); 
+        
+        if (!connection) {
+            return message.channel.send('I\'m not in a voice channel');
+        }
+
+        message.channel.send(`Playing \`${vid.snippet.title}\``);
+        
+        const ytdl = require('ytdl-core');
+        const dispatcher = connection.play(ytdl(vid.id.videoId, { filter: 'audioonly' }));
+        dispatcher.on('finish', () => {
+            dispatcher.destroy();
+            console.log('finished');
+        });        
     }
 }
