a9b77b9f339c251624f9de6a3bf3f0a9c426befc
ahyar4y <ahyar4y08@gmail.com>
2021-03-07 13:14:16 +0700

    update .leave code

--- commands/leave.js
+++ commands/leave.js
@@ -2,6 +2,10 @@ module.exports = {
     name: 'leave',
     description: 'leave',
     execute(message, args) {
-        if (message.guild.voice.channel) message.member.voice.channel.leave();
+        if (!message.guild.me.voice.channel) {
+            return message.channel.send('Wdym');
+        }
+        
+        message.guild.me.voice.channel.leave();
     }
 }
